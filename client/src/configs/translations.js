724be2c4e70602b53b34a7eb1456d961ca427858
maltenorstroem <malte.norstroem@gmail.com>
2022-01-19 15:38:21 +0100

    feat: translations

--- client/src/configs/translations.js
+++ client/src/configs/translations.js
@@ -19,4 +19,64 @@ export const Translations = {
       },
     },
   },
+  it: {
+    todos: {
+      item: {
+        description: { label: 'Description', placeholder: 'Insert your item description' },
+        duedate: { label: 'Due Date', placeholder: 'set due date' },
+        id: { label: 'Identification' },
+      },
+    },
+    web: {
+      core: {
+        illustration: {
+          fatalservererror: {
+            titletext: 'Connection not possible',
+            subtitletext:
+              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
+          },
+        },
+      },
+    },
+  },
+  de: {
+    todos: {
+      item: {
+        description: { label: 'Description', placeholder: 'Insert your item description' },
+        duedate: { label: 'Due Date', placeholder: 'set due date' },
+        id: { label: 'Identification' },
+      },
+    },
+    web: {
+      core: {
+        illustration: {
+          fatalservererror: {
+            titletext: 'Connection not possible',
+            subtitletext:
+              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
+          },
+        },
+      },
+    },
+  },
+  de_ch: {
+    todos: {
+      item: {
+        description: { label: 'Description', placeholder: 'Insert your item description' },
+        duedate: { label: 'Due Date', placeholder: 'set due date' },
+        id: { label: 'Identification' },
+      },
+    },
+    web: {
+      core: {
+        illustration: {
+          fatalservererror: {
+            titletext: 'Connection not possible',
+            subtitletext:
+              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
+          },
+        },
+      },
+    },
+  },
 };
