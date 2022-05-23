15db04c81096be7b684c509058f8725232c0a9fa
maltenorstroem <malte.norstroem@gmail.com>
2022-05-23 11:15:44 +0200

    chore: Strategic maintenance, upgrade dependencies

--- client/src/configs/translations.js
+++ client/src/configs/translations.js
@@ -27,21 +27,21 @@ export const Translations = {
     todos: {
       item: {
         description: {
-          label: 'Description',
-          placeholder: 'Insert your item description',
+          label: 'Descrizione',
+          placeholder: 'Inserite la descrizione del vostro elemento',
           constraints: { min: 'Inserisci una descrizione (almeno 12 caratteri)' },
         },
-        duedate: { label: 'Due Date', placeholder: 'set due date' },
-        id: { label: 'Identification' },
+        duedate: { label: 'data di scadenza', placeholder: 'impostare una data di scadenza' },
+        id: { label: 'Identificazione' },
       },
     },
     web: {
       core: {
         illustration: {
           fatalservererror: {
-            titletext: 'Connection not possible',
+            titletext: 'Connessione non possibile',
             subtitletext:
-              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
+              'Controllare la connessione a Internet. In caso contrario, provare a ricaricare. Se non si riesce a risolvere il problema, contattare l\'amministratore.',
           },
         },
       },
@@ -51,21 +51,21 @@ export const Translations = {
     todos: {
       item: {
         description: {
-          label: 'Description',
-          placeholder: 'Insert your item description',
+          label: 'Beschreibung',
+          placeholder: 'Erfassen Sie eine Beschreibung',
           constraints: { min: 'Bitte erfasse eine Beschreibung (mind. 12 Zeichen lang)' },
         },
-        duedate: { label: 'Due Date', placeholder: 'set due date' },
-        id: { label: 'Identification' },
+        duedate: { label: 'Fälligkeitsdatum', placeholder: 'Fälligkeitsdatum festlegen' },
+        id: { label: 'Identifikation' },
       },
     },
     web: {
       core: {
         illustration: {
           fatalservererror: {
-            titletext: 'Connection not possible',
+            titletext: 'Verbindung nicht möglich',
             subtitletext:
-              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
+              'Überprüfen Sie Ihre Internetverbindung. Wenn dies nicht der Fall ist, versuchen Sie, die Seite neu zu laden. Wenn dies immer noch nicht hilft, wenden Sie sich an Ihren Administrator.',
           },
         },
       },
@@ -75,81 +75,21 @@ export const Translations = {
     todos: {
       item: {
         description: {
-          label: 'Description',
-          placeholder: 'Insert your item description',
+          label: 'Beschreibung',
+          placeholder: 'Erfassen Sie eine Beschreibung',
           constraints: { min: 'Bitte erfasse eine Beschreibung (mind. 12 Zeichen lang)' },
         },
-        duedate: { label: 'Due Date', placeholder: 'set due date' },
-        id: { label: 'Identification' },
+        duedate: { label: 'Fälligkeitsdatum', placeholder: 'Fälligkeitsdatum festlegen' },
+        id: { label: 'Identifikation' },
       },
     },
     web: {
       core: {
         illustration: {
           fatalservererror: {
-            titletext: 'Connection not possible',
+            titletext: 'Verbindung nicht möglich',
             subtitletext:
-              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
-          },
-        },
-      },
-    },
-  },
-  it: {
-    todos: {
-      item: {
-        description: { label: 'Description', placeholder: 'Insert your item description' },
-        duedate: { label: 'Due Date', placeholder: 'set due date' },
-        id: { label: 'Identification' },
-      },
-    },
-    web: {
-      core: {
-        illustration: {
-          fatalservererror: {
-            titletext: 'Connection not possible',
-            subtitletext:
-              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
-          },
-        },
-      },
-    },
-  },
-  de: {
-    todos: {
-      item: {
-        description: { label: 'Description', placeholder: 'Insert your item description' },
-        duedate: { label: 'Due Date', placeholder: 'set due date' },
-        id: { label: 'Identification' },
-      },
-    },
-    web: {
-      core: {
-        illustration: {
-          fatalservererror: {
-            titletext: 'Connection not possible',
-            subtitletext:
-              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
-          },
-        },
-      },
-    },
-  },
-  de_ch: {
-    todos: {
-      item: {
-        description: { label: 'Description', placeholder: 'Insert your item description' },
-        duedate: { label: 'Due Date', placeholder: 'set due date' },
-        id: { label: 'Identification' },
-      },
-    },
-    web: {
-      core: {
-        illustration: {
-          fatalservererror: {
-            titletext: 'Connection not possible',
-            subtitletext:
-              'Check your internet connection. If this is not the case, try reloading. If this still does not help, contact your administrator.',
+              'Überprüfen Sie Ihre Internetverbindung. Wenn dies nicht der Fall ist, versuchen Sie, die Seite neu zu laden. Wenn dies immer noch nicht hilft, wenden Sie sich an Ihren Administrator.',
           },
         },
       },
